6217f4f586a341d3a53d079cfc6b91d5f63d4f87
alhaos <52729006+alhaos@users.noreply.github.com>
2020-12-06 06:11:55 +0500

    Update code_part_2.ps1

--- day2/code_part_2.ps1
+++ day2/code_part_2.ps1
@@ -1,5 +1,8 @@
+param(
+    $inputFileName = "D:\repository\AdventOfCode\day2\input.txt"
+)
 $i = 0
-Get-Content D:\library\AdventOfCode\day2\input.txt | ForEach-Object {
+Get-Content $inputFileName | ForEach-Object {
     $row = $_ -split ' '
     $firstIndex = [int]($row -split '-')[0]
     $secondIndex = [int]($row -split '-')[1]
