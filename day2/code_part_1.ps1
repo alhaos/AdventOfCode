ee43e7b385f5b56f5649572b25a3dd3e674a426f
alhaos <52729006+alhaos@users.noreply.github.com>
2020-12-06 06:10:36 +0500

    Update code_part_1.ps1

--- day2/code_part_1.ps1
+++ day2/code_part_1.ps1
@@ -1,11 +1,14 @@
+param(
+    $inputFileName = "D:\repository\AdventOfCode\day2\input.txt"
+)
 $i = 0
-Get-Content D:\library\AdventOfCode\day2\input.txt | ForEach-Object {
+Get-Content $inputFileName | ForEach-Object {
     $row = $_ -split ' '
     $firstIndex = [int]($row -split '-')[0]
     $secondIndex = [int]($row -split '-')[1]
     $letter = $row[1].TrimEnd(":")
     $letersArray = $row[2]
-    write-host $letersArray, $letter, $firstIndex, $letersArray[$firstIndex-1] $secondIndex, $letersArray[$secondIndex-1]
+    #write-host $letersArray, $letter, $firstIndex, $letersArray[$firstIndex-1] $secondIndex, $letersArray[$secondIndex-1]
     if ($letersArray[$firstIndex-1] -eq $letter -xor $letersArray[$secondIndex-1] -eq $letter)
     {
         $i++
