4f03eb9fe95b5e541ce781cb0346aeeee9c2c1b0
alhaos <52729006+alhaos@users.noreply.github.com>
2020-12-08 10:11:30 +0500

    day8

--- 07_Handy_Haversacks/code_part_2.ps1
+++ 07_Handy_Haversacks/code_part_2.ps1
@@ -1,3 +1,14 @@
 param(
-    $inputFileName = "D:\repository\AdventOfCode\day6\input.txt" 
+    $inputFileName =  "D:\repository\AdventOfCode\07_Handy_Haversacks\input.txt" 
 )
+
+Get-Content $inputFileName | % {
+    $t = ([regex]"^([\w\s]+) bags contain ([\w\s]+)\sbags?.$").Matches($_).Groups[1,2].Value
+    "header: {0}" -f $t[0]
+    $t[1] -split ", " | ForEach-Object{
+        $count, $color = ([regex]"(\d+)\s([\w\s]+)\sbags?").Matches($_).Groups[1,2]
+    }
+}
+
+
+
---
+++ 08_Handheld_Halting/code_part_2.ps1
@@ -0,0 +1,4 @@
+param(
+    $inputFileName = "D:\repository\AdventOfCode\07_08_Handheld_Halting\input.txt" 
+)
+$parents = @()
